40d56c012cd711c8ca0f1bc6fa3951d89e61e785
Sergjio97 <sergiocizmja@gmail.com>
2021-11-10 00:18:33 +0100

    esercizio completo

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
 const main = document.querySelector(".container");
 
+let f = "fizz";
+
+let b = "buzz";
+
+let x = f + b;
+
 for (i = 1; i <= 100; i++){
     if ((i % 3 == 0) && (i % 5 == 0)){
-        main.innerHTML += `<div class = "box red">${i}</div>`;
+        main.innerHTML += `<div class = "box red">${x}</div>`;
     } else if (i % 3 == 0){        
-        main.innerHTML += `<div class = "box green">${i}</div>`;
+        main.innerHTML += `<div class = "box green">${f}</div>`;
     } else if (i % 5 == 0){
-        main.innerHTML += `<div class = "box yellow">${i}</div>`;
+        main.innerHTML += `<div class = "box yellow">${b}</div>`;
     } else {
         main.innerHTML += `<div class = "box blue">${i}</div>`;
     }
