cb5f73c4d592da1d78eb70e52ba76dea29187930
Sergjio97 <sergiocizmja@gmail.com>
2021-11-10 00:13:01 +0100

    senza fizz e buzz

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
-const boxXl = document.querySelector(".container");
+const main = document.querySelector(".container");
 
 for (i = 1; i <= 100; i++){
-    boxXl.innerHTML += `<div class = "box">${i}</div>`
+    if ((i % 3 == 0) && (i % 5 == 0)){
+        main.innerHTML += `<div class = "box red">${i}</div>`;
+    } else if (i % 3 == 0){        
+        main.innerHTML += `<div class = "box green">${i}</div>`;
+    } else if (i % 5 == 0){
+        main.innerHTML += `<div class = "box yellow">${i}</div>`;
+    } else {
+        main.innerHTML += `<div class = "box blue">${i}</div>`;
+    }
 
 }
